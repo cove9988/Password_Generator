67593d7168574451a1c88f44ddfd7e4072186c8a
paulg <cove9988@gmail.com>
2020-10-02 10:45:31 +1000

    Update PyPassGen.py
    randint(min, max) including the max, so for example, if lower case chr(123) will be  "{"

--- PyPassGen.py
+++ PyPassGen.py
@@ -21,27 +21,27 @@ while count < length:
     rand = random.randint (0,3)
     if rand == 0:
         lower += 1
-        b = int(random.randint (97,123))
+        b = int(random.randint (97,122))
         password.append(b)
     elif rand == 1:
         upper += 1
-        b = random.randint (65,91)
+        b = random.randint (65,90)
         password.append(b)
     elif rand == 2:
         number += 1
-        b = random.randint (48,58)
+        b = random.randint (48,57)
         password.append(b)
     elif rand == 3:
         r = random.randint(0,2)
         symbol += 1
         if r == 0:
-            b = random.randint (33,48)
+            b = random.randint (33,47)
             password.append(b)
         elif r == 1:
-            b = random.randint (91,97)
+            b = random.randint (91,96)
             password.append(b)
         elif r == 2:
-            b = random.randint (123,126)
+            b = random.randint (123,125)
             password.append(b)
     count += 1
 
